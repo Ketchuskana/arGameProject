64f3b7b3a7d68c503f1de0e7a05629b829c62234
Ketchuskana <105216628+Ketchuskana@users.noreply.github.com>
2026-04-21 00:40:40 +0200

    commande vocal fonctionnelle

--- voice_control.py
+++ voice_control.py
@@ -1,31 +1,51 @@
-import speech_recognition as sr
+import queue
+import json
 import threading
+import pyaudio
+from vosk import Model, KaldiRecognizer
 
 class VoiceControl:
     def __init__(self):
         self.command = None
-        self.recognizer = sr.Recognizer()
+        self.q = queue.Queue()
+
+        # modèle Vosk
+        self.model = Model("model")
+        self.rec = KaldiRecognizer(self.model, 16000)
+
+        # PyAudio
+        self.p = pyaudio.PyAudio()
+
+        self.stream = self.p.open(
+            format=pyaudio.paInt16,
+            channels=1,
+            rate=16000,
+            input=True,
+            frames_per_buffer=8000
+        )
+
+        self.stream.start_stream()
+
+        # thread écoute
         self.running = True
+        self.thread = threading.Thread(target=self.listen, daemon=True)
+        self.thread.start()
 
-        thread = threading.Thread(target=self.listen_loop)
-        thread.daemon = True
-        thread.start()
-
-    def listen_loop(self):
-        try:
-            mic = sr.Microphone()
-        except:
-            print("⚠️ Micro non détecté")
-            return
-
+    def listen(self):
         while self.running:
-            try:
-                with mic as source:
-                    audio = self.recognizer.listen(source, phrase_time_limit=2)
+            data = self.stream.read(4000, exception_on_overflow=False)
 
-                text = self.recognizer.recognize_google(audio).lower()
-                print("🎤", text)
-                self.command = text
+            if self.rec.AcceptWaveform(data):
+                result = json.loads(self.rec.Result())
+                text = result.get("text", "").lower()
 
-            except:
-                self.command = None
+                print("🎤 VOICE:", text)
+
+                if "boost" in text:
+                    self.command = "boost"
+                elif "speed" in text:
+                    self.command = "speed"
+                elif "restart" in text:
+                    self.command = "restart"
+                elif "quit" in text:
+                    self.command = "quit"
